c26b0e8fc2e7f7f42258cc277b7577abd0facc09
vathsalya <vathsalya1999@gmail.com>
2018-10-03 16:21:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/srinivasrao/scrollview/ViewPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.srinivasrao.scrollview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer [] images={R.mipmap.login,R.mipmap.login,R.mipmap.login,R.mipmap.login,R.mipmap.login};
+    public ViewPagerAdapter(Context context){
+        this.context=context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject( @NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+    @NonNull
+    @Override
+    public Object instantiateItem( @NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout,null);
+        ImageView imageview = (ImageView) view.findViewById(R.id.imageView);
+        imageview.setImageResource(images[position]);
+
+        ViewPager vp=(ViewPager) container;
+        vp.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        ViewPager vp=(ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+
+    }
+}
